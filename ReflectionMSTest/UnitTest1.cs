d30c5cb1a4648f0309669714747f7297e960d5bb
NoorMdKhan123 <noormd6400@gmail.com>
2021-11-02 22:19:57 +0530

    Done with parametrized constructot

--- ReflectionMSTest/UnitTest1.cs
+++ ReflectionMSTest/UnitTest1.cs
@@ -7,13 +7,23 @@ namespace ReflectionMSTest
     public class UnitTest1
     {
         [TestMethod]
-       //UC 4.1
+        //UC 4.1
         public void MoodAnalyserProblem_ReturnMoodAnalyseObject()
         {
             string message = null;
             object expected = new MoodAnalyser(message);
             object obj = MoodAnalyserFactory.CreateMoodAnalyse
                 ("ReflectionMood.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
+        }
+
+        //UC 5.1 Given Mood Analyser class name should return mood analyser object
+        public void MoodAnalyserClassNameReturn_MoodAnalyserObj_WithParametrizedConstructor()
+        {
+            object expected = new MoodAnalyser("HAPPY");
+            object obj =
+               MoodAnalyserFactory.CreateMoodAnalyseUsingPrametrizedConstructor("ReflectionMood.MoodAnalyser",
+                "MoodAnalyser", "HAPPY");
             expected.Equals(obj);
         }
     }
--- ReflectionMood/MoodAnalyserFactory.cs
+++ ReflectionMood/MoodAnalyserFactory.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Reflection;
 using System.Text.RegularExpressions;
-
-
-
 namespace ReflectionMood
 {
   public class MoodAnalyserFactory
     {
+        //UC5 for parameterized constructor by passing message paramter to class method
+        public static object CreateMoodAnalyseUsingPrametrizedConstructor(string className, string constructorName, string message)
+
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string)});
+                    object instance = ctor.Invoke(new object[] { "HAPPY"});
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.
+                        ExceptionType.NO_SUCH_CLASS, "Class not found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.
+                    ExceptionType.NO_SUCH_METHOD, "Cosntructor is not found");
+            }
+        }
         //UC4 CreateMA Method to create object of MA class
         public static object CreateMoodAnalyse
             (string className, string constructorName)
